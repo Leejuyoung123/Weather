08f68065f3d20d14a530dc03aeff3218ca6d4120
admin <admin@DESKTOP-EBOP39N>
2021-05-04 19:40:54 +0900

    호ㅓㅏ면에뿌리기

---
+++ src/main/java/com/weather/controller/HomeController.java
@@ -0,0 +1,45 @@
+package com.weather.controller;
+
+import java.net.URLEncoder;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import com.weather.model.WeatherVO;
+
+
+
+@Controller
+@ComponentScan
+public class HomeController {
+	
+	private final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+	private final String apiKey = "382337faac5360f4627378b171bb335c";  // 발급받은 API key
+	@GetMapping("weather")
+    @RequestMapping("weather")
+	public String weatherApi(Model model,@ModelAttribute("weatherVO")WeatherVO weatherVO) throws Exception {
+		StringBuilder urlBuilder = new StringBuilder(BASE_URL);
+		try {
+			    urlBuilder.append("?" + URLEncoder.encode("lat", "UTF-8") + "=35");
+			    urlBuilder.append("&" + URLEncoder.encode("lon", "UTF-8") + "=139");
+			    urlBuilder.append("&" + URLEncoder.encode("appid", "UTF-8") + "=" + apiKey);
+			    urlBuilder.append("&" + URLEncoder.encode("lang", "UTF-8") + "=kr");
+			    urlBuilder.append("&" + URLEncoder.encode("units", "UTF-8") + "=metric");
+	
+			    RestTemplate restTemplate = new RestTemplate();
+			    weatherVO = restTemplate.getForObject(urlBuilder.toString(), WeatherVO.class);
+			 
+			} 	catch (Exception e) {
+		  		e.printStackTrace();
+		  	}
+
+			model.addAttribute("weatherVO",weatherVO);
+			return "weather";
+	}
+}
